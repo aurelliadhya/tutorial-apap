5c30f5b10d1e7849ccce862d94573384db1d006e
aurelliadhya <aurel.da99@gmail.com>
2019-09-11 14:20:47 +0700

    `feat: tutorial 1 -

---
+++ hitungumur/src/main/java/controller/AgeController.java
@@ -0,0 +1,24 @@
+package controller;
+import apap.tutorial.hitungumur.model.Age;
+import model.Age;
+import org.springframework.boot.Banner;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.time.LocalDate;
+import java.util.Optional;
+
+public class AgeController {
+
+    private String getHitungUmurPage(Optional<LocalDate> birthdate, Model model) {
+        if(birthdate.isPresent()) {
+            final Age currentAge = new Age(birthdate.get());
+            model.addAttribute("age", currentAge);
+            model.addAttribute("birthdate", birthdate.get());
+        } else {
+            model.add
+        }
+    }
+}
