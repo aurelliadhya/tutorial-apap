a7072b6aa36dc3f204992d3c609a729f837ad680
aurelliadhya <aurel.da99@gmail.com>
2019-11-06 22:17:25 +0700

    feat: add tutorial 7 files

--- gopud/src/main/java/apap/tutorial/gopud/controller/MenuController.java
+++ gopud/src/main/java/apap/tutorial/gopud/controller/MenuController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 @Controller
---
+++ gopud/src/main/java/apap/tutorial/gopud/controller/UserController.java
@@ -0,0 +1,65 @@
+package apap.tutorial.gopud.controller;
+
+import apap.tutorial.gopud.model.UserModel;
+import apap.tutorial.gopud.service.RoleService;
+import apap.tutorial.gopud.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private RoleService roleService;
+
+    @RequestMapping(value = "/addUser", method = RequestMethod.POST)
+    private String addUserSubmit(@ModelAttribute UserModel user) {
+        String password = user.getPassword();
+        Boolean containtsNumber = false;
+        Boolean containtsLetter = false;
+
+        if (password.length() < 8) {
+            return "invalid-password";
+        } else {
+            for (int i=0;i<password.length();i++) {
+                if (Character.isDigit(password.charAt(i))) {
+                    containtsNumber = true;
+                } if (Character.isAlphabetic(password.charAt(i))) {
+                    containtsLetter = true;
+                }
+            }
+        }
+
+        if (!(containtsNumber = true) || !(containtsLetter = true)) {
+            return "invalid-password";
+        }
+
+        userService.addUser(user);
+        return "home";
+    }
+
+    @RequestMapping(value = "/updatePassword", method = RequestMethod.POST)
+    private String updatePasswordSubmit(@ModelAttribute UserModel user, String newPassword, String oldPassword, String confirmPassword, Model model) {
+        UserModel existingUser = userService.getUserByUsername(user.getUsername());
+
+        model.addAttribute("listRole", roleService.findAll());
+
+        if (!userService.isMatching(oldPassword, existingUser.getPassword())) {
+            return "update-password-failed";
+        } if (!newPassword.equals(confirmPassword)) {
+            return "update-password-failed";
+        }
+
+        existingUser.setPassword(newPassword);
+        userService.addUser(existingUser);
+        return "update-password-success";
+    }
+}
